0aaf45d4c6f94fc2fb49150475d69355815ee473
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2016-05-18 14:41:07 +0200

    Basic tear-down at node setup working

--- topological_roslaunch/scripts/tear_down_at_nodes.py
+++ topological_roslaunch/scripts/tear_down_at_nodes.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env python
 
-from __future__ import with_statement 
 import rospy
-
-class TearDownAtNodes(object):
-	""" 
-	"""
-	def __init__(self, tear_down_nodes = []):
-		super(TearDownAtNodes, self).__init__()
-		
-		self.tear_down_nodes = set(tear_down_nodes)
-		self.launch_files_up = False		
-        
-        rospy.Subscriber('current_node', String, self._update_topological_location, queue = 1)
-
-    def _update_topological_location(self, node):
-    	if node.data in self.tear_down_nodes:
-    		if self.launch_files_up:
-    			rospy.loginfo('Tearing down launch files ')
-    	elif not self.launch_files_up:
-    		rospy.loginfo('Bringing launch files up')
+from topological_roslaunch import TearDownAtNodes
+import yaml
+import sys
 
 if __name__ == '__main__':
 
-	tear_down_nodes = ['ChargingPoint', 'WayPoint1']
+    rospy.init_node('tear_down_at_nodes')
 
-    executor = TearDownAtNodes(tear_down_nodes = tear_down_nodes)        
+    try:
+        config_file =  rospy.get_param("~config")
+    except:
+        rospy.logerr("Could not get config parameter. Make sure to supply a YAML "
+                     "configuration file.")
+        sys.exit(1)
+    
+    with open(config_file, "r") as f:
+        config = yaml.load(f.read())
+
+    for c in config:
+        # rospy.loginfo()
+        executor = TearDownAtNodes(tear_down_nodes = c['nodes'], launch_files = c['tear_down'])        
+
+    
     rospy.spin()
